2e99848291c1788d487ad061d0622dc88eb6d6a3
KipandaJr <antoniokipandacardoso@gmail.com>
2025-03-06 02:50:05 +0100

    :construction: fix: simple changes aplied

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -10,57 +10,56 @@ import { SOCIAL_LINK } from './ social-link.data';
 const Footer: FC = () => {
   return (
     <Box
-      pt="xl"
       gap="s"
-      px="2xl"
       as="footer"
       width="100%"
-      position="relative"
       alignItems="center"
-      flexDirection="column"
+      px={['s', 's', 's', '2xl']}
+      pb={['m', 'm', 'm', '2xl']}
+      mb={['6rem', '6rem', '6rem', 'unset']}
       justifyContent="space-between"
+      pt={['2xl', '2xl', '2xl', 'l']}
       display={['flex', 'flex', 'flex', 'grid']}
-      pb={['6rem', '6rem', '6rem', '2xl', '2xl']}
-      gridTemplateColumns={['1fr', '1fr', '1fr', '1fr auto 1fr']}
+      gridTemplateColumns={['1fr', '1fr', '1fr', '1fr auto']}
     >
-      <Link
-        target="_blank"
-        rel="noreferrer"
-        title="Visit our landing page"
-        href="https://www.interestprotocol.com/"
-      >
-        <Box
-          width="2.5rem"
-          height="2.5rem"
-          color="onSurface"
-          position="relative"
-          nHover={{ color: 'primary' }}
+      <Box display="flex" alignItems="center" gap="m">
+        <Link
+          target="_blank"
+          rel="noreferrer"
+          title="Visit our landing page"
+          href="https://www.interestprotocol.com/"
         >
-          <IPXSVG maxHeight="100%" maxWidth="100%" width="100%" />
-        </Box>
-      </Link>
+          <Box
+            width="1.7rem"
+            height="1.7rem"
+            color="onSurface"
+            position="relative"
+            nHover={{ color: 'primary' }}
+          >
+            <IPXSVG maxHeight="100%" maxWidth="100%" width="100%" />
+          </Box>
+        </Link>
+        <Typography
+          size="medium"
+          variant="label"
+          color="onSurface"
+          textTransform="capitalize"
+          display={['none', 'none', 'none', 'block']}
+        >
+          &copy; Interest PROTOCOL {new Date().getFullYear()}
+        </Typography>
+      </Box>
       <Box
-        gap="m"
+        gap="xs"
         display="flex"
         position="relative"
         flexDirection="column-reverse"
       >
-        <Box textAlign="center" pb="m">
-          <Typography
-            size="medium"
-            variant="label"
-            color="onSurface"
-            textTransform="capitalize"
-          >
-            &copy; Interest PROTOCOL {new Date().getFullYear()}
-          </Typography>
-        </Box>
         <Box
           gap="xs"
           display="flex"
           justifySelf="end"
           alignItems="center"
-          flexDirection="column"
           justifyContent="center"
         >
           <Typography color="onSurface" variant="label" size="medium">
@@ -76,16 +75,17 @@ const Footer: FC = () => {
                 title={`Follow us on ${title}`}
               >
                 <Box
-                  p="xs"
-                  width="2.5rem"
-                  height="2.5rem"
+                  p="2xs"
+                  width="2rem"
+                  height="2rem"
                   color="onSurface"
                   border="1px solid"
                   borderRadius="full"
+                  display="flex"
                   borderColor="outlineVariant"
                   nHover={{ borderColor: 'outline' }}
                 >
-                  <Icon maxHeight="100%" maxWidth="100%" width="100%" />
+                  <Icon maxHeight="1.5rem" maxWidth="1.5rem" width="1.5rem" />
                 </Box>
               </Link>
             ))}
