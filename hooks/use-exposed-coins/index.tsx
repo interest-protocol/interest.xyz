176fc4d726e8927eda0261bda98e0ea29d970418
Marco <marcomorete73@hotmail.com>
2025-03-08 18:39:09 +0100

    :sparkles: feat: add coins to expose cached

--- hooks/use-exposed-coins/index.tsx
+++ hooks/use-exposed-coins/index.tsx
@@ -1,47 +1,44 @@
 import { AccountAddress } from '@aptos-labs/ts-sdk';
-import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 
 import { TOKENS } from '@/constants/coins';
 import { TokenPrice, TokenWithPrice } from '@/interface';
 import { formatDollars } from '@/utils';
 
 const useExposedCoins = () => {
-  const [exposedCoins, setExposedCoins] = useState<
-    ReadonlyArray<TokenWithPrice>
-  >([]);
+  const { data: exposedCoins, ...rest } = useSWR(
+    'coins-to-expose',
+    async () => {
+      const prices: ReadonlyArray<TokenPrice> = await fetch(
+        `https://rates-api-staging.up.railway.app/api/fetch-quote?${TOKENS.map((coin) => `coins=${coin.address.toString()}`).join('&')}`,
+        {
+          method: 'GET',
+          headers: { network: 'MOVEMENT' },
+        }
+      ).then((res) => res.json());
 
-  useEffect(() => {
-    fetch(
-      `https://rates-api-staging.up.railway.app/api/fetch-quote?${TOKENS.map((coin) => `coins=${coin.address.toString()}`).join('&')}`,
-      {
-        method: 'GET',
-        headers: { network: 'MOVEMENT' },
-      }
-    )
-      .then((res) => res.json())
-      .then((data: ReadonlyArray<TokenPrice>) => {
-        const coinsToExpose = TOKENS.reduce((acc, coin) => {
-          const item = data.find((item) =>
-            coin.address.equals(AccountAddress.from(item.coin))
-          );
+      const coinsToExpose = TOKENS.reduce((acc, coin) => {
+        const item = prices.find((item) =>
+          coin.address.equals(AccountAddress.from(item.coin))
+        );
 
-          if (!item) return acc;
+        if (!item) return acc;
 
-          return [
-            ...acc,
-            {
-              ...coin,
-              usd: formatDollars(item.price),
-              usdPrice24Change: item.priceChange24HoursPercentage,
-            },
-          ];
-        }, [] as ReadonlyArray<TokenWithPrice>);
+        return [
+          ...acc,
+          {
+            ...coin,
+            usd: formatDollars(item.price),
+            usdPrice24Change: item.priceChange24HoursPercentage,
+          },
+        ];
+      }, [] as ReadonlyArray<TokenWithPrice>);
 
-        setExposedCoins(coinsToExpose);
-      });
-  }, []);
+      return coinsToExpose;
+    }
+  );
 
-  return { exposedCoins };
+  return { exposedCoins, ...rest };
 };
 
 export default useExposedCoins;
