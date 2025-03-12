0ef3f97eb54c98150cd05f580e995ccebcb9a514
Marco <marcomorete73@hotmail.com>
2025-03-12 14:43:39 +0100

    :zap: chore: optimize balance fetching

--- lib/coins-manager/index.tsx
+++ lib/coins-manager/index.tsx
@@ -1,14 +1,13 @@
-import { GetFungibleAssetMetadataResponse } from '@aptos-labs/ts-sdk';
 import { useAptosWallet } from '@razorlabs/wallet-kit';
 import BigNumber from 'bignumber.js';
 import { type FC, useEffect, useId } from 'react';
 import useSWR from 'swr';
 
-import { isAptos } from '@/utils';
+import { parseToMetadata } from '@/utils';
 
 import { useAptosClient } from '../aptos-provider/aptos-client/aptos-client.hooks';
 import { useCoins } from './coins-manager.hooks';
-import { Asset, TokenStandard } from './coins-manager.types';
+import { Asset } from './coins-manager.types';
 
 const CoinsManager: FC = () => {
   const id = useId();
@@ -34,72 +33,15 @@ const CoinsManager: FC = () => {
           accountAddress: currentAccount.address,
         });
 
-        const coinsMetadata: Record<
-          string,
-          GetFungibleAssetMetadataResponse[0]
-        > = {};
-
-        for (const item of coinsData) {
-          const metadata = await client.getFungibleAssetMetadataByAssetType({
-            assetType: item.asset_type!,
-          });
-
-          coinsMetadata[item.asset_type!] = metadata;
-        }
-
         const coins: Record<string, Asset> = coinsData.reduce(
-          (acc, { asset_type, amount }) => {
-            if (!asset_type || !coinsMetadata[asset_type]) return acc;
-
-            const {
-              name,
-              symbol,
-              decimals,
-              token_standard,
-              icon_uri: iconUri,
-              project_uri: projectUri,
-            } = coinsMetadata[asset_type];
-
-            if (isAptos(asset_type)) {
-              const symbol = 'MOVE';
-
-              const type =
-                token_standard === TokenStandard.COIN
-                  ? '0x1::aptos_coin::AptosCoin'
-                  : '0xa';
-
-              return {
-                ...acc,
-                [type]: {
-                  type,
-                  name,
-                  symbol,
-                  decimals,
-                  balance: BigNumber(amount.toString()),
-                  standard:
-                    token_standard === TokenStandard.COIN
-                      ? TokenStandard.COIN
-                      : TokenStandard.FA,
-                  ...(!!projectUri && { projectUri }),
-                  ...(!!iconUri && { iconUri: iconUri }),
-                },
-              };
-            }
+          (acc, { asset_type, amount, metadata }) => {
+            if (!asset_type || !metadata) return acc;
 
             return {
               ...acc,
               [asset_type]: {
-                name,
-                decimals,
-                type: asset_type,
-                balance: BigNumber(amount.toString()),
-                symbol: symbol,
-                standard:
-                  token_standard === 'v1'
-                    ? TokenStandard.COIN
-                    : TokenStandard.FA,
-                ...(!!projectUri && { projectUri }),
-                ...(!!iconUri && { iconUri: iconUri }),
+                ...parseToMetadata(metadata),
+                balance: BigNumber(amount),
               },
             };
           },
