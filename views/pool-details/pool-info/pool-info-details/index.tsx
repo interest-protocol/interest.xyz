a093c7c103f52ed8cdfd1fed6a4b9768f37f21de
KipandaJr <antoniokipandacardoso@gmail.com>
2025-04-24 10:25:57 +0100

    :sparkles: fix: Hide Statistics on curve

--- views/pool-details/pool-info/pool-info-details/index.tsx
+++ views/pool-details/pool-info/pool-info-details/index.tsx
@@ -2,7 +2,7 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 import Accordion from '../components/accordion';
-import { POOL_INFORMATION, POOL_STATISTICS } from '../pool-info.data';
+import { POOL_INFORMATION } from '../pool-info.data';
 import PoolInfoDetailsBalance from './pool-info-details-balance';
 import PoolInfoDetailsPool from './pool-info-details-pool';
 import PoolInfoDetailsStats from './pool-info-details-stats';
@@ -12,12 +12,8 @@ const PoolDetails: FC = () => (
     <Accordion title={POOL_INFORMATION.title}>
       <PoolInfoDetailsPool />
     </Accordion>
-    <Accordion title="Live balance">
-      <PoolInfoDetailsBalance />
-    </Accordion>
-    <Accordion title={POOL_STATISTICS.title} noBorder>
-      <PoolInfoDetailsStats />
-    </Accordion>
+    <PoolInfoDetailsBalance />
+    <PoolInfoDetailsStats />
   </Box>
 );
 
