3836ebc0e2779a639a18a18c7caaa21a7c6d5ec3
KipandaJr <antoniokipandacardoso@gmail.com>
2025-05-07 00:01:33 +0100

    :lipstick: fix: Card Height on hover fixed

--- views/pools/pool-card/pool-card-header.tsx
+++ views/pools/pool-card/pool-card-header.tsx
@@ -8,15 +8,14 @@ import { TAG_COLOR } from './pool-card.data';
 import { PoolCardHeaderProps } from './pool-card.types';
 
 const PoolCardHeader: FC<PoolCardHeaderProps> = ({ tags }) => (
-  <Box display="flex" alignItems="center" justifyContent="space-between">
-    <Box display="flex" flexWrap="wrap" alignItems="center">
+  <Box display="flex" justifyContent="space-between" height="1.7rem">
+    <Box display="flex" flexWrap="wrap">
       {tags.map((tag) =>
         tag ? (
           <Tag
             key={v4()}
             px="0"
             py="0"
-            mb="2xs"
             mr="2xs"
             size="small"
             height="1.4rem"
@@ -35,10 +34,11 @@ const PoolCardHeader: FC<PoolCardHeaderProps> = ({ tags }) => (
       )}
     </Box>
     <Button
-      mb="auto"
       isIcon
-      display="none"
+      mb="auto"
+      p="0.125rem"
       variant="text"
+      display="none"
       color="onSurface"
       className="arrow-wrapper"
       nHover={{ bg: 'surface' }}
