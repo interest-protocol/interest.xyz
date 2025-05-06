452fc32e724dacb1900cb13fe7bfd9a504021728
KipandaJr <antoniokipandacardoso@gmail.com>
2025-05-06 10:01:24 +0100

    :fire: fix: Replace rewardsPerSecond to Zero

--- views/pools/pool-card/index.tsx
+++ views/pools/pool-card/index.tsx
@@ -64,9 +64,9 @@ const PoolCurveCard: FC<PoolCardProps> = ({ pool }) => {
         )
       : 0;
 
-  const apr = farms?.[0]?.rewards.map(({ rewardFa, rewardsPerSecond }) =>
+  const apr = farms?.[0]?.rewards.map(({ rewardFa }) =>
     FixedPointMath.toNumber(
-      BigNumber(String(rewardsPerSecond))
+      BigNumber(String(0))
         .times(
           prices?.find(({ coin }) => coin === MOVE.address.toString())?.price ??
             0
