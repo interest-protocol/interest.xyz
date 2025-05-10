dada2248613599bf16c97c907acf16d3b49b3786
KipandaJr <antoniokipandacardoso@gmail.com>
2025-05-10 18:13:05 +0100

    :sparkles: feat: Adding Icon on LP Coin

--- views/pool-details/pool-form/pool-form-deposit/pool-form-deposit-receive.tsx
+++ views/pool-details/pool-form/pool-form-deposit/pool-form-deposit-receive.tsx
@@ -1,17 +1,24 @@
+import { Network } from '@interest-protocol/interest-aptos-v2';
 import { Box, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import Skeleton from 'react-loading-skeleton';
 
+import { TokenIcon } from '@/components';
+import { useNetwork } from '@/lib/aptos-provider/network/network.hooks';
+import { TokenStandard } from '@/lib/coins-manager/coins-manager.types';
 import { IPoolForm } from '@/views/pools/pools.types';
 
 import { usePoolDetails } from '../../pool-details.context';
 
 const PoolFormDepositReceive: FC = () => {
   const { loading } = usePoolDetails();
+  const network = useNetwork<Network>();
   const { control } = useFormContext<IPoolForm>();
   const value = useWatch({ control, name: 'lpCoin.value' });
   const symbol = useWatch({ control, name: 'lpCoin.symbol' });
+  const iconURI = useWatch({ control, name: 'lpCoin.iconUri' });
+  const standard = useWatch({ control, name: 'lpCoin.standard' });
 
   return (
     <Box>
@@ -26,9 +33,18 @@ const PoolFormDepositReceive: FC = () => {
           alignItems="center"
           justifyContent="space-between"
         >
-          <Typography variant="body" size="large">
-            {loading ? <Skeleton width="4rem" /> : symbol}
-          </Typography>
+          <Box display="flex" alignItems="center" gap="m">
+            <TokenIcon
+              withBg
+              url={iconURI}
+              symbol={symbol}
+              network={network}
+              rounded={standard === TokenStandard.COIN}
+            />
+            <Typography variant="body" size="large">
+              {loading ? <Skeleton width="4rem" /> : symbol}
+            </Typography>
+          </Box>
           <Typography variant="body" ml="m" size="large">
             {loading ? <Skeleton width="2rem" /> : value || 0}
           </Typography>
