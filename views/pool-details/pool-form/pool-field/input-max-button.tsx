44d48aaf98b82fbbf2a416cb048714bd34459269
Antonio Kipanda Cardoso <34079870+KipandaJr@users.noreply.github.com>
2025-05-26 09:18:47 +0100

    :bug: fix max button on sync
    * :bug: fix: max button sync fixed
    
    * :fire: fix: replace imports

--- views/pool-details/pool-form/pool-field/input-max-button.tsx
+++ views/pool-details/pool-form/pool-field/input-max-button.tsx
@@ -1,4 +1,6 @@
+import { VolatilePool } from '@interest-protocol/interest-aptos-curve';
 import { Button } from '@interest-protocol/ui-kit';
+import BigNumber from 'bignumber.js';
 import { FC } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 
@@ -7,17 +9,21 @@ import { useCoins } from '@/lib/coins-manager/coins-manager.hooks';
 import { isAptos, ZERO_BIG_NUMBER } from '@/utils';
 import { IPoolForm } from '@/views/pools/pools.types';
 
-import { NameProps } from './pool-field.types';
+import { usePoolDetails } from '../../pool-details.context';
+import { MaxButtonProps } from './pool-field.types';
 
-const MaxButton: FC<NameProps> = ({ name }) => {
+const MaxButton: FC<MaxButtonProps> = ({ name, index }) => {
   const { coinsMap } = useCoins();
-  const { setValue, control } = useFormContext<IPoolForm>();
+  const { loading, pool } = usePoolDetails();
+  const { setValue, getValues, control } = useFormContext<IPoolForm>();
 
   const token = useWatch({ control, name });
 
   const balance = coinsMap[token.type]?.balance ?? ZERO_BIG_NUMBER;
 
   const handleMax = () => {
+    if (loading || !pool) return;
+
     setValue(`lpCoin.locked`, false);
     setValue(`tokenList.0.locked`, false);
     setValue(`tokenList.1.locked`, false);
@@ -33,14 +39,59 @@ const MaxButton: FC<NameProps> = ({ name }) => {
       return;
     }
 
-    setValue(`${name}.valueBN`, value);
-    setValue(
-      `${name}.value`,
-      FixedPointMath.toNumber(
+    if (getValues('syncBalances')) {
+      const amount = FixedPointMath.toNumber(
         value.decimalPlaces(0, 1),
         token.decimals
-      ).toString()
-    );
+      ).toString();
+
+      const newIndex = +(index != 1);
+
+      setValue(`tokenList.${index}.value`, amount);
+      setValue(
+        `tokenList.${index}.valueBN`,
+        FixedPointMath.toBigNumber(
+          amount,
+          getValues(`tokenList.${index}.decimals`)
+        )
+      );
+
+      if (pool.curve == 'stable') {
+        setValue(`tokenList.${newIndex}.value`, amount);
+        setValue(
+          `tokenList.${newIndex}.valueBN`,
+          FixedPointMath.toBigNumber(
+            amount,
+            getValues(`tokenList.${newIndex}.decimals`)
+          )
+        );
+      } else {
+        const poolExtraData = pool.poolExtraData as unknown as VolatilePool;
+        const priceRaw = poolExtraData.prices[pool.tokensAddresses[1]]?.price;
+        const price = FixedPointMath.toNumber(BigNumber(String(priceRaw)), 18);
+
+        const newAmount = String(
+          (newIndex ? +amount / price : +amount * price).toFixed(4)
+        );
+        setValue(`tokenList.${newIndex}.value`, newAmount);
+        setValue(
+          `tokenList.${newIndex}.valueBN`,
+          FixedPointMath.toBigNumber(
+            newAmount,
+            getValues(`tokenList.${newIndex}.decimals`)
+          )
+        );
+      }
+    } else {
+      setValue(
+        `${name}.value`,
+        FixedPointMath.toNumber(
+          value.decimalPlaces(0, 1),
+          token.decimals
+        ).toString()
+      );
+      setValue(`${name}.valueBN`, value);
+    }
   };
 
   return (
