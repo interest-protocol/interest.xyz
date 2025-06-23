436cacb986b281e7f595d3a6f4821010ca1a1384
luyandra.branco <luyandrabranco@gmail.com>
2025-06-23 21:24:15 +0100

    ♻️ refactor: add efects on sidebar and items sidebar

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,36 +1,39 @@
-import { Box } from '@interest-protocol/ui-kit';
+import { Box, Motion } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 import SidebarClosed from './components/SidebarClosed';
 import SidebarContent from './components/SidebarContent';
 import { ISidebarProps } from './sidebar.types';
 
-const Sidebar: FC<ISidebarProps> = ({ onClose }) => {
-  return (
-    <Box
-      top="0"
-      left="0"
-      width="100%"
-      display="flex"
-      pt="1.875rem"
-      pl="1.875rem"
-      bg="#0000004D"
-      position="fixed"
-      flexDirection="column"
-      backdropFilter="blur(10px)"
+const Sidebar: FC<ISidebarProps> = ({ onClose }) => (
+  <Box
+    top="0"
+    left="0"
+    width="100%"
+    display="flex"
+    pt="1.875rem"
+    pl="1.875rem"
+    bg="#0000004D"
+    position="fixed"
+    flexDirection="column"
+    backdropFilter="blur(10px)"
+  >
+    <Motion
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.7, ease: 'easeInOut' }}
     >
       <Box
         display="flex"
         flexDirection="row"
-        transition="transform 0.3s ease-in-out"
         width={['100%', '100%', '100%', '18rem']}
         height={['100vh', '100vh', '100vh', '60rem']}
       >
         <SidebarContent />
         <SidebarClosed onClose={onClose} />
       </Box>
-    </Box>
-  );
-};
+    </Motion>
+  </Box>
+);
 
 export default Sidebar;
