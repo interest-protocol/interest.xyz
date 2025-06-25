aa1df4468b5bff795552f4e32ca1a483aa99d3c0
luyandra.branco <luyandrabranco@gmail.com>
2025-06-25 20:14:19 +0100

    🐛 fix: bug starting project

--- views/swap/input/header-info.tsx
+++ views/swap/input/header-info.tsx
@@ -1,24 +1,13 @@
 import { Box, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
-import { FormProvider, useFormContext, useWatch } from 'react-hook-form';
+import { useFormContext, useWatch } from 'react-hook-form';
 
-import { useModal } from '@/hooks/use-modal';
-
-import SwapSettingsForm from '../swap-settings-form';
 import { InputProps } from './input.types';
 
 const HeaderInfo: FC<InputProps> = ({ label }) => {
-  const { setModal, handleClose } = useModal();
   const form = useFormContext();
 
   const symbol = useWatch({ control: form.control, name: `${label}.symbol` });
-
-  setModal(
-    <FormProvider {...form}>
-      <SwapSettingsForm />
-    </FormProvider>,
-    { isOpen: true, custom: true, onClose: handleClose }
-  );
 
   return (
     <Box
