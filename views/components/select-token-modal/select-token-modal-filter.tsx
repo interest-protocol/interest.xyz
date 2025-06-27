ee1e199fb9f821a7233bdd6dbd10444278de4164
luyandra.branco <luyandrabranco@gmail.com>
2025-06-27 22:12:09 +0100

    ♻️ refactor: improvement component select token modal

--- views/components/select-token-modal/select-token-modal-filter.tsx
+++ views/components/select-token-modal/select-token-modal-filter.tsx
@@ -20,24 +20,38 @@ const SelectTokenFilter: FC<SelectTokenFilterProps> = ({
   const filterSelected = useWatch({ control, name: 'filter' });
 
   return (
-    <Box
-      mt="s"
-      gap="s"
-      display="grid"
-      flexWrap="wrap"
-      gridTemplateColumns="1fr 1fr"
-    >
+    <Box gap="s" display="grid" flexWrap="wrap" gridTemplateColumns="1fr 1fr">
       {[TokenOrigin.Strict, TokenOrigin.Wallet].map((item) => (
         <Box
           key={v4()}
           cursor="pointer"
           onClick={() => setValue('filter', item)}
         >
-          <Typography variant="body" size="medium" textAlign="center" py="m">
+          <Typography
+            py="m"
+            size="medium"
+            variant="body"
+            fontWeight="400"
+            textAlign="center"
+            fontFamily="Inter"
+            color={filterSelected === item ? '#FFFFFF' : '#9CA3AF'}
+          >
             {ORIGIN_TITLE[item]}
           </Typography>
           {filterSelected === item && (
-            <Motion layout borderBottom="2px solid" borderColor="primary" />
+            <Motion
+              layout
+              py="8px"
+              px="95px"
+              mt={'-21%'}
+              bg="#B4C5FF33"
+              color="#FFFFFF"
+              height="2.75rem"
+              fontWeight="500"
+              maxWidth="232px"
+              fontFamily="Inter"
+              borderRadius="9999px"
+            />
           )}
         </Box>
       ))}
