23499867f7a62c867b1289e364fca5a087ee5e23
luyandra.branco <luyandrabranco@gmail.com>
2025-07-08 20:34:55 +0100

    ✨ feat: form bridge

--- views/swap/components/bridge/index.tsx
+++ views/swap/components/bridge/index.tsx
@@ -2,8 +2,7 @@ import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 import Input from '../input';
-import SwapFormFieldSlider from '../input/swap-manager-slider';
-import ToInput from '../input/to-input';
+import SelectorInput from '../input/selector-input';
 import SwapButton from '../swap-button';
 import SwapFlipToken from '../swap-flip-token';
 import SwapManager from '../swap-manager';
@@ -14,31 +13,25 @@ const Bridge: FC = () => (
       <Box
         py="l"
         px="xl"
+        mb="4px"
         display="flex"
-        bg="container"
+        bg="#9CA3AF1A"
         borderRadius="s"
         flexDirection="column"
       >
         <Input label="from" />
-        <SwapFormFieldSlider />
       </Box>
-      <Box
-        my="-1.4rem"
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-      >
-        <SwapFlipToken />
-      </Box>
-      <Box
-        py="s"
-        px="xl"
-        display="flex"
-        bg="container"
-        borderRadius="s"
-        flexDirection="column"
-      >
-        <ToInput />
+      <Box display="flex" flexDirection="column">
+        <SelectorInput label="from" />
+        <Box
+          my="-1.4rem"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <SwapFlipToken type="bridge" />
+        </Box>
+        <SelectorInput label="to" />
       </Box>
       <SwapButton />
     </Box>
--- views/swap/components/swap-tabs/index.tsx
+++ views/swap/components/swap-tabs/index.tsx
@@ -1,13 +1,48 @@
+import { Box } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
+import { FormProvider, useFormContext } from 'react-hook-form';
 
+import { CogsSVG } from '@/components/svg';
 import Tabs from '@/components/tabs';
+import { useModal } from '@/hooks/use-modal';
 import { useTabState } from '@/hooks/use-tab-manager';
+
+import SwapSettingsForm from '../swap-settings-form';
 
 const SwapTabs: FC = () => {
   const { tab, setTab } = useTabState();
   const tabs = ['Swap', 'Bridge'];
 
-  return <Tabs tabs={tabs} setTab={setTab} tab={tab} />;
+  const { setModal, handleClose } = useModal();
+  const form = useFormContext();
+
+  const handleOpenSettings = () =>
+    setModal(
+      <FormProvider {...form}>
+        <SwapSettingsForm />
+      </FormProvider>,
+      { isOpen: true, custom: true, onClose: handleClose }
+    );
+
+  return (
+    <Box display="flex" alignItems="center" justifyContent="space-between">
+      <Tabs tabs={tabs} setTab={setTab} tab={tab} />
+      <Box
+        role="button"
+        lineHeight="0"
+        display="flex"
+        cursor="pointer"
+        color="onSurface"
+        alignItems="center"
+        aria-label="Settings"
+        onClick={handleOpenSettings}
+        transition="transform 500ms ease-in-out"
+        nHover={{ transform: 'rotate(180deg)' }}
+      >
+        <CogsSVG maxWidth="1.25rem" maxHeight="1.25rem" width="100%" />
+      </Box>
+    </Box>
+  );
 };
 
 export default SwapTabs;
