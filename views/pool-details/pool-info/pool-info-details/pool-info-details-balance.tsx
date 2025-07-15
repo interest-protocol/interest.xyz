02e2b0c119b8935a7c66a1698e7e642d93435658
KipandaJr <antoniokipandacardoso@gmail.com>
2025-07-15 19:15:18 +0100

    :bug: fix: ration on stable pool when its on sync mode

--- views/pool-details/pool-info/pool-info-details/pool-info-details-balance.tsx
+++ views/pool-details/pool-info/pool-info-details/pool-info-details-balance.tsx
@@ -1,11 +1,13 @@
 import { normalizeSuiAddress } from '@interest-protocol/interest-aptos-v2';
 import BigNumber from 'bignumber.js';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
 import { v4 } from 'uuid';
 
 import { FixedPointMath } from '@/lib';
 import { AssetMetadata } from '@/lib/coins-manager/coins-manager.types';
 import { formatMoney } from '@/utils';
+import { IPoolForm } from '@/views/pools/pools.types';
 
 import { usePoolDetails } from '../../pool-details.context';
 import Accordion from '../components/accordion';
@@ -13,7 +15,32 @@ import ItemStandard from '../components/accordion/item-standard';
 import PoolInfoLoading from '../pool-info-loading';
 
 const PoolInfoDetailsBalance: FC = () => {
+  const { setValue } = useFormContext<IPoolForm>();
   const { pool, loading } = usePoolDetails();
+
+  useEffect(() => {
+    if (!pool || pool.curve != 'stable') return;
+
+    if (loading) return;
+
+    const ratio = pool.tokensAddresses?.map(
+      (address, index) =>
+        +FixedPointMath.toNumber(
+          BigNumber(String(pool.balances?.[index] ?? 0)),
+          pool.algorithm === 'curve'
+            ? 18
+            : (metadata?.[normalizeSuiAddress(address)]?.decimals ?? 8)
+        )
+    );
+    const [token0Balance, token1Balance] = ratio;
+
+    const hasNoLiquidity = token0Balance < 100 && token1Balance < 100;
+
+    setValue('ratio', [
+      hasNoLiquidity ? 1 : token1Balance / token0Balance,
+      hasNoLiquidity ? 1 : token0Balance / token1Balance,
+    ]);
+  }, [loading, pool]);
 
   if (!pool || loading)
     return (
