0c87febadfaf8d8a83092d4025b92840ea00f458
luyandra.branco <luyandrabranco@gmail.com>
2025-07-27 16:03:08 +0100

    ✨ feat: search and select component

--- views/stats/components/stats-tabs/index.tsx
+++ views/stats/components/stats-tabs/index.tsx
@@ -1,6 +1,7 @@
-import { Box } from '@interest-protocol/ui-kit';
+import { Box, TextField } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
+import { SearchSVG } from '@/components/svg';
 import Tabs from '@/components/tabs';
 import { useTabState } from '@/hooks/use-tab-manager';
 
@@ -18,8 +19,44 @@ const StatsTabs: FC = () => {
       flexDirection={['column', 'row']}
     >
       <Tabs tabs={tabs} setTab={setTab} tab={tab} />
-      <Box display="flex" gap="0.5rem">
-        asasasasasa
+      <Box gap="0.75rem" display="flex" flexDirection={['column', 'row']}>
+        <Box
+          px="0.5rem"
+          as="select"
+          border="none"
+          bg="#9CA3AF1A"
+          color="#6B7280"
+          height="2.75rem"
+          cursor="pointer"
+          fontSize="0.875rem"
+          borderRadius="0.75rem"
+          width={['100%', '8.875rem']}
+        >
+          <option value="1w">1W volume</option>
+          <option value="1d">1D volume</option>
+          <option value="1m">1M volume</option>
+        </Box>
+
+        <TextField
+          fontSize="medium"
+          placeholder="Search pools"
+          nPlaceholder={{ opacity: 0.7 }}
+          fieldProps={{
+            height: '2.75rem',
+            borderRadius: 'xs',
+            bg: '#9CA3AF1A',
+            color: '#6B7280',
+            borderColor: '#9CA3AF1A',
+          }}
+          Suffix={
+            <SearchSVG
+              width="100%"
+              maxWidth="1rem"
+              maxHeight="1rem"
+              color="#6B7280"
+            />
+          }
+        />
       </Box>
     </Box>
   );
