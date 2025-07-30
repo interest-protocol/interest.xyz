3311f09c14ce357e4c97cd19e08d0febfb2d2e5f
Marco <marcomorete73@hotmail.com>
2025-07-30 09:34:22 +0100

    :bug: fix: remove leading zeros

--- views/swap/swap-manager/index.tsx
+++ views/swap/swap-manager/index.tsx
@@ -44,7 +44,7 @@ const SwapManager: FC = () => {
     const slippage = (getValues('settings.slippage') * 100).toFixed(0);
 
     fetch(
-      `https://api.mosaic.ag/v1/quote?srcAsset=${removeLeadingZeros(from.type)}&dstAsset=${removeLeadingZeros(to.type)}&amount=${from.valueBN.toFixed(0)}&feeInBps=${EXCHANGE_FEE_BPS}&feeReceiver=${TREASURY}&includeSources=interest_v2,interest_curve_stable,interest_curve_volatile&slippage=${slippage}&sender=${account?.address ?? '0x0'}`,
+      `https://api.mosaic.ag/v1/quote?srcAsset=${removeLeadingZeros(from.type)}&dstAsset=${removeLeadingZeros(to.type)}&amount=${from.valueBN.toFixed(0)}&feeInBps=${EXCHANGE_FEE_BPS}&feeReceiver=${TREASURY}&includeSources=interest_curve_stable,interest_curve_volatile&slippage=${slippage}&sender=${account?.address ?? '0x0'}`,
       {
         headers: {
           accept: '*/*',
