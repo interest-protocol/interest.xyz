b848251a9fd4cd56ceeab638bd2a6e124ea1587e
luyandra.branco <luyandrabranco@gmail.com>
2025-08-05 00:10:20 +0100

    🔥 refactor: removed farm info

--- constants/farms.ts
+++
@@ -1,6 +0,0 @@
-import { FARMS } from '@interest-protocol/interest-aptos-curve';
-
-export const FARMS_BY_LP = FARMS.reduce(
-  (acc, farm) => ({ ...acc, [farm.stakeFa]: farm }),
-  {} as Record<string, (typeof FARMS)[0]>
-);
--- constants/routes.ts
+++ constants/routes.ts
@@ -4,11 +4,6 @@
  */
 export enum RoutesEnum {
   Swap = 'swap',
-  Pools = 'pools',
-  Analytics = 'analytics',
-  PoolCreate = 'pool-create',
-  Leaderboard = 'leaderboard',
-  PoolDetails = 'pool-details',
   TokenCreate = 'token-create',
 }
 
@@ -18,10 +13,5 @@ export enum RoutesEnum {
  */
 export const Routes: Record<RoutesEnum, string> = {
   [RoutesEnum.Swap]: '/',
-  [RoutesEnum.Pools]: '/pools',
-  [RoutesEnum.Analytics]: '/analytics',
-  [RoutesEnum.Leaderboard]: '/leaderboard',
-  [RoutesEnum.PoolCreate]: '/pools/create',
   [RoutesEnum.TokenCreate]: '/create-token',
-  [RoutesEnum.PoolDetails]: '/pools/details',
 };
