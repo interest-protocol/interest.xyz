bc37c491d81e2122b434a00c0d124391bc0f7879
luyandra.branco <luyandrabranco@gmail.com>
2025-08-04 23:20:22 +0100

    🔥 refactor: removed pool title bar

--- views/components/pool-title-bar/pool-title-bar.types.ts
+++
@@ -1,9 +0,0 @@
-import { Dispatch, SetStateAction } from 'react';
-
-import { PoolDetailsMode } from '@/views/pool-details/pool-details.types';
-export interface PoolTitleBarProps {
-  loading?: boolean;
-  onBack: () => void;
-  centerTile?: boolean;
-  modeState: [PoolDetailsMode, Dispatch<SetStateAction<PoolDetailsMode>>];
-}
