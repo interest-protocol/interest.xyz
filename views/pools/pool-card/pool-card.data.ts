ca159db840eab66e0df4464a6eee95a67f456b4c
KipandaJr <antoniokipandacardoso@gmail.com>
2025-08-11 12:01:19 +0100

    :fire: fix: Adding tooltips on tags

--- views/pools/pool-card/pool-card.data.ts
+++ views/pools/pool-card/pool-card.data.ts
@@ -3,6 +3,10 @@ export const TAG_COLOR: Record<string, Record<'color' | 'bg', string>> = {
     color: '#88f',
     bg: 'transparent',
   },
+  concentrated: {
+    color: '#88f',
+    bg: 'transparent',
+  },
   v2: {
     color: '#a4f',
     bg: 'transparent',
@@ -19,4 +23,13 @@ export const TAG_COLOR: Record<string, Record<'color' | 'bg', string>> = {
     color: 'onSuccessContainer',
     bg: 'successContainer',
   },
+};
+
+export const TAG_TOOLTIP: Record<string, string> = {
+  curve: 'Concentrated Liquidity without \nconstant active management.',
+  concentrated: `Concentrated Liquidity without \nconstant active management.`,
+  v2: 'v2',
+  volatile: 'Designed for uncorrelated assets.',
+  stable: 'Stable',
+  earn: 'Earn MOVE when \nproviding liquidity.',
 };
